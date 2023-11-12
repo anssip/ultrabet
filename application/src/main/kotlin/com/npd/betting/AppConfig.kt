419b82f7d586e9efb37b1753565c98e6c01ab4e7
Anssi Piirainen <api@iki.fi>
2023-11-12 11:46:42 +0200

    Delete empty line :-)

--- application/src/main/kotlin/com/npd/betting/AppConfig.kt
+++ application/src/main/kotlin/com/npd/betting/AppConfig.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.graphql.execution.RuntimeWiringConfigurer
 
-
 @Configuration
 open class GraphQlConfig {
   @Bean
